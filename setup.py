394cf845aec6602c8ac1a561d97fb439f6545a4e
AntoninRousset <AntoninRousset@users.noreply.github.com>
2019-12-07 19:57:44 +0100

    setup

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/AntoninRousset/escaperoom',
-    packages=['escaperoom']
+    packages=['escaperoom'],
     scripts=['escaperoom-master'],
     classifiers=[
         'Development Status :: 3 - Alpha',
