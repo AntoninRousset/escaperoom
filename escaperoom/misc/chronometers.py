ec7f0cc1bd5a2c8523750443c11983c264f490a5
AntoninRousset <AntoninRousset@users.noreply.github.com>
2020-02-25 10:28:43 +0100

    timeout (not tested)

--- escaperoom/misc/chronometers.py
+++ escaperoom/misc/chronometers.py
@@ -13,12 +13,12 @@
 from abc import ABC
 from datetime import datetime, timedelta
 
-from . import Misc
+from . import asyncio, Misc
 
 
 class Chronometer(Misc):
 
-    def __init__(self, name='__main'):
+    def __init__(self, name):
         super().__init__(name)
         self.start_time = None #TODO list of plays and pauses
         self.end_time = None
@@ -27,12 +27,17 @@ class Chronometer(Misc):
     def __str__(self):
         return f'chronometer "{self.name}"'
 
-    def start(self):
-        self.start_time = datetime.today()
+    async def start(self):
+        async with self.changed:
+            self.start_time = datetime.today()
+            self.changed.notify_all()
 
-    def stop(self):
-        self.end_time = datetime.today()
+    async def stop(self):
+        async with self.changed:
+            self.end_time = datetime.today()
+            self.changed.notify_all()
 
+    @property
     def elapsed(self):
         if self.start_time is None:
             return timedelta(0)
@@ -43,7 +48,44 @@ class Chronometer(Misc):
         else:
             raise RuntimeError()
 
-    def is_running(self):
+    @property
+    def running(self):
         return self.start_time is not None and self.end_time is None
 
 
+class Timeout(Chronometer):
+
+    def __init__(self, name, max_time):
+        super().__init__(name)
+        if not isinstance(time, timedelta):
+            time = timedelta(time)
+        self._max_time = max_time
+        asyncio.create_task(self._timeout_checking())
+
+    def __bool__(self):
+        return self.elapsed() < self._max_time
+
+    async def _timeout_checking(self):
+        while True:
+            async with self.changed:
+                if self:
+                    try:
+                        wt = self.remaining().total_seconds()
+                        await asyncio.wait_for(self.changed.wait(), timeout=wt)
+                    except TimeoutError:
+                        pass
+                else:
+                    self.changed.notify_all()
+                    while self:
+                        await self.changed.wait()
+
+    async def set_max_time(self, value):
+        async with self.changed:
+            self._max_time = value
+            self.notify_all()
+
+    @property
+    def remaining(self):
+        if not self:
+            return timedelta()
+        return self._max_time - self.elapsed()
