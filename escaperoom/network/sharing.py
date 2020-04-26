c8134856997eae2682f057abfd075386a22c5740
AntoninRousset <AntoninRousset@users.noreply.github.com>
2020-04-26 12:00:19 +0200

    better lock?

--- escaperoom/network/sharing.py
+++ escaperoom/network/sharing.py
@@ -12,7 +12,7 @@
 
 import json
 import re
-from base64 import b64decode
+from base64 import b64encode, b64decode
 from collections import defaultdict
 
 from . import asyncio, Network
@@ -23,14 +23,16 @@ from ..subprocess import SubProcess
 class Cluster(Network):
 
     _locks = dict()
+    _conditions = dict()
 
     class _Etcdctl():
 
-        def __init__(self, *args, env=dict(), wait_terminate=False):
+        #TODO? TXN
+
+        def __init__(self, *args, env=dict()):
             self.args = ['etcdctl', '--write-out=json', *args]
             self.env = {'ETCDCTL_API': '3'}
             self.env.update(env)
-            self.wait_terminate = wait_terminate
 
             self.sp = None
 
@@ -48,9 +50,7 @@ class Cluster(Network):
             return self
 
         async def __aexit__(self, exc_type, exc, tb):
-            w = self.terminate()
-            if self.wait_terminate:
-                await w
+            self.terminate()
 
         def _decode(self, resp):
             def decode_kv(kv):
@@ -66,6 +66,8 @@ class Cluster(Network):
                 for event in data['Events']:
                     if 'kv' in event:
                         decode_kv(event['kv'])
+                    if 'prev_kv' in event:
+                        decode_kv(event['prev_kv'])
             return data
 
         async def communicate(self, msg=None):
@@ -79,7 +81,12 @@ class Cluster(Network):
             while True:
                 p = self.sp.stdout
                 line = await asyncio.wait_for(p.readline(), timeout=timeout)
-                yield self._decode(line)
+                try:
+                    if line:
+                        yield self._decode(line)
+                except json.JSONDecodeError:
+                    print(f'could not decode response: {line}')
+                    print(await self.sp.stderr.read())
 
         def terminate(self):
             try:
@@ -88,17 +95,28 @@ class Cluster(Network):
                 pass
             return asyncio.create_task(self.sp.wait())
 
-    class _Lock():
+    class _Condition():
 
-        def __init__(self, key):
+        def __init__(self, key, lock=None, *, loop=None):
+            if lock is None:
+                lock = Cluster.Lock(key)
+            elif lock.key != key:
+                raise ValueError('lock argument must point to the same key')
+
             self.key = key
 
-            self._client = None
-            self._lease_id = None
-            self._locked = asyncio.Event()
-            self._underlying_lock = asyncio.Lock()
+            self._underlying_cond = asyncio.Condition(lock=lock, loop=loop)
+            self._loop = self._underlying_cond._loop
+            self.locked = self._underlying_cond.locked
 
-            asyncio.create_task(self._watching(self.lock_key))
+            self._waiters = 0
+            self._loop_countdown = 0
+
+            self._waiting_line = Cluster.Lock(self.notify_key)
+            self._first_in_line = asyncio.Event()
+            self._notification = asyncio.Event()
+            asyncio.create_task(self._notify_watching())
+            asyncio.create_task(self._notification_watching())
 
         async def __aenter__(self):
             await self.acquire()
@@ -106,77 +124,74 @@ class Cluster(Network):
         async def __aexit__(self, exc_type, exc, tb):
             self.release()
 
-        async def _watching(self, lock_key):
-            async def read_ttl(lease_id):
-                async with Cluster._Etcdctl('lease', 'timetolive',
-                                            lease_id) as client:
-                    async for r in client.responses():
-                        if 'ttl' in r:
-                            return int(r['ttl'])
-                        return None
-
-            ttl = None
-            async for lease_id in Cluster.watch(lock_key, timeout=ttl):
-                self._lease_id = lease_id
-                if lease_id is None:
-                    self._locked.clear()
-                else:
-                    self._locked.set()
-                    try:
-                        ttl = await read_ttl(lease_id)
-                    except Exception as e:  # TMP?, may fail
-                        print('error reading ttl:', repr(e))
-
         async def acquire(self):
-            await self._underlying_lock.acquire()
-            try:
-                self._client = await Cluster._Etcdctl('lock', str(self.key))
-                async for r in self._client.responses():
-                    key, lease_id = r['kvs'][0]['key'].rsplit('/', 1)
-                    if key == self.key:
-                        await Cluster.set(self.lock_key, lease_id)
-                        return await self._locked.wait()
-            except Exception:
-                if self._client is not None:
-                    self._client.terminate()
-                self._underlying_lock.release()
-                raise
+            while True:
+                # TODO
+                return await self._underlying_cond.acquire()
+                # DEBUG
+                print('### WTF 1:', self._underlying_cond.locked())
+                try:
+                    if int(await Cluster.get(self.notify_key, 0)) == 0:
+                        return
+                    self._underlying_cond.release()
+                except Exception:
+                    self._underlying_cond.release()
+                    raise
 
-        async def _release(self):
-            await self._client.terminate()
-            await Cluster.rem(self.lock_key)
-            self._underlying_lock.release()
+        async def wait(self):
+            t = asyncio.create_task(self._underlying_cond.wait())
+            self._waiters += 1
+            async with self._waiting_line:
+                print('### WTF 2:', self._waiting_line.locked(), flush=True)
+                self._first_in_line.set()
+                await t
+            self._waiters -= 1
+
+        async def _notification_watching(self):
+            while True:
+                await self._first_in_line.wait()
+                await self._notification.wait()
+                async with self._underlying_cond:
+                    if self._loop_countdown == 0:
+                        await Cluster.set(self.notify_key, str(0))
+                    else:
+                        notify = int(await Cluster.get(self.notify_key, 0))
+                        self._underlying_cond.notify(1)
+                        self._loop_countdown -= 1
+                        await Cluster.set(self.notify_key, str(notify-1))
+                self._first_in_line.clear()
+
+        async def _notify_watching(self):
+            async for new, old in Cluster.watch(self.notify_key, 0):
+                if int(new) > int(old) or (int(new) < 0 and int(old) >= 0):
+                    self._loop_countdown = self._waiters
+                if int(new) == 0:
+                    self._notification.clear()
+                else:
+                    self._notification.set()
+
+        async def _notify(self, n):
+            notify = int(await Cluster.get(self.notify_key, 0))
+            if n >= 0:
+                await Cluster.set(self.notify_key, str(notify+n))
+            else:
+                await Cluster.set(self.notify_key, str(-1))
+
+        def notify(self, n=1):
+            if not self._underlying_cond.locked():
+                raise RuntimeError('lock not acquired')
+            return asyncio.create_task(self._notify(n))
+
+        def notify_all(self):
+            self.notify(-1)
 
         def release(self):
-            if not self.locked():
-                raise RuntimeError('releasing unacquired lock')
-            if self._client is None:
-                raise RuntimeError('cannot release stranger\'s lock')
-            return asyncio.create_task(self._release())
-
-        def locked(self):
-            return self._locked.is_set()
+            # ensure notify is written
+            self._underlying_cond.release()
 
         @property
-        def lock_key(self):
-            return self.key + '_lock'
-
-        @property
-        def lease_id(self):
-            return self._lease_id if self.locked() else None
-
-    class Condition():
-
-        def __init__(self, key, lock=None):
-            if lock is None:
-                lock = Cluster.Lock(key)
-            elif lock.key != key:
-                raise ValueError('lock argument must point to the same key')
-
-            self._underlying_condition = asyncio.Condition(lock=lock)
-            self.locked = self._underlying_condition.locked
-            self.acquire = self._underlying_condition.acquire
-            self.release = self._underlying_condition.release
+        def notify_key(self):
+            return self.key + '_notify'
 
     @classmethod
     def normalize_key(cls, key):
@@ -318,30 +333,38 @@ class Cluster(Network):
                 raise RuntimeError(f'cannot remove value: {repr(e)}')
 
     @classmethod
-    async def watch(cls, key: str, *, first=True, timeout=None):
+    async def watch(cls, key: str, default=None, *, first=True, timeout=None):
         if not key:
             raise RuntimeError('key cannot be empty')
 
-        async with Cluster._Etcdctl('watch', str(key)) as client:
+        async with Cluster._Etcdctl('watch', '--prev-kv', str(key)) as client:
             if first:
                 try:
-                    yield await Cluster.get(key)
+                    yield await Cluster.get(key, default=default), default
                 except KeyError:
                     pass
 
             async for r in client.responses(timeout=timeout):
                 for event in r['Events']:
                     if 'kv' in event:
+                        if 'prev_kv' in event:
+                            prev_kv = event['prev_kv']
+                            if 'key' in prev_kv and prev_kv['key'] == key:
+                                value = prev_kv.get('value')
+                                old = default if value is None else value
+                        else:
+                            old = default
                         kv = event['kv']
                         if 'key' in kv and kv['key'] == key:
-                            yield kv.get('value')
+                            value = kv.get('value')
+                            yield default if value is None else value, old
 
     @classmethod
-    def Lock(cls, key):
+    def Condition(cls, key):
         key = cls.normalize_key(key)
-        if key not in cls._locks:
-            cls._locks[key] = cls._Lock(key)
-        return cls._locks[key]
+        if key not in cls._conditions:
+            cls._conditions[key] = cls._Condition(key)
+        return cls._conditions[key]
 
     @property
     def _etcd_listen_client_urls(self):
@@ -368,22 +391,112 @@ class Cluster(Network):
         return ','.join(peers)
 
 
-class Shared():
+class _Lock():
 
-    def __init__(self, name):
-        self.name = name
-        self.__changed = asyncio.Condition()
-        #self.__etcd_lock = Cluster.get().client.Lock(self.path)
+    def __init__(self, key, *, loop=None):
+        self.key = key
+        self._loop = loop
 
-    async def __aenter__(self):
-        await self.__changed.__aenter__()
-        #await asyncio.run_in_executor(None, self.__etcd_lock.acquire)
-        print(self.__etcd_lock.is_acquired)
+        asyncio.create_task(self._watching(self.lock_key))
 
-    async def __aexit__(self, exc_type, exc, tb):
-        #self.__etcd_lock.release()
-        await self.__changed.__aexit__(exc_type, exc, tb)
+        self._lease_id = None
+        self._locked = asyncio.Event()
+        self._underlying_lock = asyncio.Lock()
+
+    async def _watching(self, lock_key):
+        async def read_ttl(lease_id):
+            async with Cluster._Etcdctl('lease', 'timetolive',
+                                        lease_id) as client:
+                async for r in client.responses():
+                    if 'ttl' in r:
+                        return int(r['ttl'])
+                    return None
+
+        ttl = None
+        async for lease_id, _ in Cluster.watch(lock_key, timeout=ttl):
+            self._lease_id = lease_id
+            if lease_id is None:
+                self._locked.clear()
+            else:
+                self._locked.set()
+                try:
+                    ttl = await read_ttl(lease_id)
+                except Exception as e:  # TMP?, may fail
+                    print('error reading ttl:', repr(e))
+
+    def locked(self):
+        return self._locked.is_set()
 
     @property
-    def path(self):  # TODO
-        return '/shared/' + self.name + '/'
+    def lock_key(self):
+        return self.key + '_lock'
+
+    @property
+    def lease_id(self):
+        return self._lease_id if self.locked() else None
+
+
+class Lock():
+
+    _locks = dict()
+
+    def __init__(self, key, *, loop=None):
+        if loop is None:
+            loop = asyncio.get_event_loop()
+
+        key = Cluster.normalize_key(key)
+        if key not in Lock._locks:
+            Lock._locks[key] = _Lock(key)
+        self._lock = Lock._locks[key]
+
+        self.key = self._lock.key
+        self._client = None
+
+    async def __aenter__(self):
+        await self.acquire()
+
+    async def __aexit__(self, exc_type, exc, tb):
+        self.release()
+
+    async def acquire(self):
+        try:
+            async with self._lock._underlying_lock:
+                self._client = await Cluster._Etcdctl('lock', str(self.key))
+                async for r in self._client.responses():
+                    key, lease_id = r['kvs'][0]['key'].rsplit('/', 1)
+                    if key == self.key:
+                        await Cluster.set(self.lock_key, lease_id)
+                        return await self._lock._locked.wait()
+        except Exception:
+            if self._client is not None:
+                self._client.terminate()
+            raise
+
+    async def _release(self, client):
+        # await Cluster.rem(self.lock_key)
+        await client.terminate()
+
+    def release(self):
+        if not self.locked():
+            raise RuntimeError('Cannot release unacquired lock')
+        if self._client is None:
+            raise RuntimeError('Cannot release locked acquired by a stranger')
+        asyncio.create_task(self._release(self._client))
+        self._lock._locked.clear()
+        self._client = None
+
+    def locked(self):
+        return self._lock.locked()
+
+    @property
+    def lock_key(self):
+        return self._lock.lock_key
+
+    @property
+    def lease_id(self):
+        return self._lock.lease_id
+
+
+class Shared:
+    pass
+
