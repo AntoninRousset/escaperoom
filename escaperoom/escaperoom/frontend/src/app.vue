2defda2f0ff9eac8ce26525b39aa0282a6f79248
AntoninRousset <AntoninRousset@users.noreply.github.com>
2020-12-23 19:43:56 +0100

    recursive node for states

--- escaperoom/escaperoom/frontend/src/app.vue
+++ escaperoom/escaperoom/frontend/src/app.vue
@@ -1,10 +1,11 @@
 <template>
-	<e-node>
+	<e-node :states="states">
 	</e-node>
 </template>
 
 <script>
 import 'normalize.css'
+import {mapState} from 'vuex'
 
 import ENode from './components/node.vue'
 
@@ -18,7 +19,11 @@ export default {
 	},
 	created() {
 		this.$store.commit('fetch_states');
-	}
+	},
+	computed: mapState({
+		states: 'states',
+		darkMode: 'darkMode'
+	}),
 }
 </script>
 
--- escaperoom/escaperoom/frontend/src/components/node.vue
+++ escaperoom/escaperoom/frontend/src/components/node.vue
@@ -1,6 +1,6 @@
 <template>
-	States:
 	<div v-for="state in states">
+		<e-node :states="state.children"></e-node>
 		{{state.name}}
 	</div>
 </template>
@@ -11,7 +11,7 @@ import {mapState} from 'vuex'
 
 export default {
 	name: 'ENode',
-	props: [],
+	props: ['states'],
 	data() {
 		return {
 		}
@@ -19,7 +19,7 @@ export default {
 	mounted() {
 	},
 	computed: mapState({
-		states: 'states'
+		darkMode: 'darkMode'
 	}),
 	watch: {
 		darkMode(darkMode, oldDarkMode) {
@@ -32,4 +32,9 @@ export default {
 <style lang="scss">
 	@import "../scss/colors.scss";
 
+	div {
+		border: solid;
+		padding: 4px;
+		margin: 2px;
+	}
 </style>
