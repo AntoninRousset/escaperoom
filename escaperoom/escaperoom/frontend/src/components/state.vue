3a657edd5934fa1f2a41b4c7e8d7e0649bd80041
VianneyRousset <6491145+VianneyRousset@users.noreply.github.com>
2020-12-25 17:25:50 +0100

    dynamic state size

--- escaperoom/escaperoom/frontend/src/components/state.vue
+++ escaperoom/escaperoom/frontend/src/components/state.vue
@@ -16,7 +16,7 @@
 				height: size.height + 'px',
 				width: size.width + 'px'
 			}">
-			<e-state v-for="state in info.children" :info="state" />
+			<e-state v-for="state in info.children" :info="state"/>
 		</div>
 	</div>
 </template>
@@ -27,41 +27,77 @@ import {mapState} from 'vuex'
 export default {
 	name: 'EState',
 	props: ['info'],
+
 	data() {
 		return {
 			root: null,
 			gridStep: 50,
 		}
 	},
+
 	mounted() {
+
 		this.root = this.$refs['root'];
+
+    if (this.info.id == 4) {
+      setInterval(() => {
+        this.info.x = 3;
+        this.info.y = 2;
+      }, 2000)
+    }
+
 	},
+
 	computed: {
+
 		...mapState(['fsm', 'darkMode']),
+
 		hasChildren() {
 			return Object.keys(this.info.children).length > 0;
 		},
+
 		position() {
 			return {
-				x: this.info.x*this.gridStep,
-				y: this.info.y*this.gridStep
+				x: this.info.x * this.gridStep,
+				y: this.info.y * this.gridStep
 			};
 		},
+
 		size() {
-			if (this.root === null) {
-				return {height: null, width: null};
-			}
+
+			if (this.root === null)
+				return {height: '', width: ''};
+
 			return {
 				height: this.root.scrollHeight,
 				width: this.root.scrollWidth
 			};
 		},
 	},
+
 	watch: {
-		darkMode(darkMode, oldDarkMode) {
+
+		darkMode(newValue, oldValue) {
 		},
+
+    "info.children": {
+      handler: function(newValue) {
+        console.log('Info changed');
+        console.log('>>>', this.root.style);
+        this.root.style.width = this.root.scrollWidth;
+        this.root.style.height = this.root.scrollHeight;
+      },
+      deep: true,
+    }
+
 	},
+
 	methods: {
+
+    computeSize() {
+
+    }
+
 	},
 }
 </script>
@@ -80,7 +116,7 @@ export default {
 			border: solid;
 
 			position: absolute;
-			overflow: hidden;
+			overflow: visible;
 
 		}
 
